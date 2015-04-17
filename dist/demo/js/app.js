ed3696973f7340c356650c81fc859ad9303653b6
Dimitri <dimitri.mestdagh@optis.be>
2015-04-17 21:11:05 +0200

    Adding libs and demo folder to gh-pages branch

---
+++ dist/demo/js/app.js
@@ -0,0 +1,21 @@
+(function($, hljs) {
+  $(document).ready(function() {
+    hljs.initHighlightingOnLoad();
+
+    $(".search > input").keydown(function(evt) {
+      var el = this;
+      setTimeout(function() {
+        var txt = $(el).val();
+        $(".icons .icon").each(function(idx, icon) {
+          var parent = $(icon).parent();
+          var name = parent.data("name");
+          if (name.indexOf(txt) >= 0) {
+            parent.show();
+          } else {
+            parent.hide();
+          }
+        });
+      });
+    });
+  });
+}(jQuery, hljs));
